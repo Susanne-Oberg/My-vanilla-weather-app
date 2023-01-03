8423006764a4f76e23ef3ce38621a696e1fc1695
Susanne Öberg <Susanne-Oberg@users.noreply.github.com>
2023-01-03 18:21:50 +0200

    added time

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} ${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c097o6f11c380bft35a9aaa4c76c29ad";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=c097o6f11c380bft35a9aaa4c76c29ad&units=metric";
+  "https://api.shecodes.io/weather/v1/current?query=Porvoo&key=c097o6f11c380bft35a9aaa4c76c29ad&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
